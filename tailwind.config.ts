677432b5290f5fb45876bde0cb9cb71e17b75e91
DavidCastro1229 <davidcastro505personal@gmail.com>
2025-12-23 13:47:31 -0600

    Kanban ordenes trabajo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-
 export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
@@ -12,7 +11,6 @@ export default {
         "2xl": "1400px",
       },
     },
-    
     extend: {
       colors: {
         border: "hsl(var(--border))",
